a14cc36e9ca5a0c0d0d053511590589921f85a32
dyf <843347859@qq.com>
2020-03-21 22:35:52 +0800

    change key path

--- src/include/config.h
+++ src/include/config.h
@@ -6,7 +6,7 @@
 #define ELFSIGN_CONFIG_H
 
 #define KEY_LENGTH 2048     // 密钥长度
-#define PUB_KEY_FILE "./pem/pubkey.pem" // 公钥路径
-#define PRI_KEY_FILE "./pem/prikey.pem" // 私钥路径
+#define PUB_KEY_FILE "./pubkey.pem" // 公钥路径
+#define PRI_KEY_FILE "./prikey.pem" // 私钥路径
 
 #endif //ELFSIGN_CONFIG_H
